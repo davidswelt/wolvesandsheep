1c21cf191f02daeb5773990882cb525905964928
David Reitter <david.reitter@gmail.com>
2015-04-04 16:18:46 -0400

    test-was: preserve time stamp

--- src/test-was.sh
+++ src/test-was.sh
@@ -14,6 +14,7 @@ for file in `ls players/*.jar players/*/*.jar classic.players/*.jar classic.play
 file2=`echo $file | tr [:upper:] [:lower:]` 
 mv $file $file2 
 file=$file2
+touch -r "$file" /tmp/sametime.tmp
 
 # delete javafx and was packages if necessary
 zip -d $file javafx/\*
@@ -25,6 +26,8 @@ zip -d $file sandbox2.policy test-was.sh reitter.jar run.py
 if [ "$file" != "players/reitter.jar" ]; then
    zip -d $file reitter/*
 fi
+
+touch -r /tmp/sametime.tmp "$file"
 
 allclasses="$allclasses `basename $file .jar`"
 alljars="$alljars:$file"
