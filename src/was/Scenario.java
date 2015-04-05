davidswelt/wolvesandsheep

commit 0f48de04984adf6d9087988fe437629b4f4d3356

Throw new CoordinatesOutOfBoundsException Check coordinates in getIndex

src/was/Scenario.java
@@ -308,20 +308,23 @@ public class Scenario {
     }
 
     Player setFigure(Class p, int x, int y) {
-        GameLocation l = loc(x, y);
-        if (l != null) {
-            if (l.x > 0 && l.x < tmpGb.getCols() && l.y > 0 && l.y < tmpGb.getRows()) {
-                try {
+        try {
+            GameLocation l = loc(x, y);
+            if (l != null) {
+                if (x > 0 && x < tmpGb.getCols() && y > 0 && y < tmpGb.getRows()) {
+
                     if (tmpGb.isEmptyCell(l.x, l.y)) {
                         return tmpGb.addPlayer((Player) p.newInstance(), l);
                     }
-                } catch (IndexOutOfBoundsException ex) {
-                } catch (InstantiationException ex) {
-                    Logger.getLogger(Scenario.class.getName()).log(Level.SEVERE, null, ex);
-                } catch (IllegalAccessException ex) {
-                    Logger.getLogger(Scenario.class.getName()).log(Level.SEVERE, null, ex);
+
                 }
             }
+        } catch (CoordinatesOutOfBoundsException ex) {
+        } catch (IndexOutOfBoundsException ex) {
+        } catch (InstantiationException ex) {
+            Logger.getLogger(Scenario.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            Logger.getLogger(Scenario.class.getName()).log(Level.SEVERE, null, ex);
         }
         return null;
     }
@@ -372,7 +375,6 @@ public class Scenario {
                 sy = -1;
             }
         }
-
 
         double x = x1;
         double y = y1;

src/was/CoordinatesOutOfBoundsException.java
@@ -1,0 +1,10 @@
+
+package was;
+
+/**
+ *
+ * @author dr
+ */
+public class CoordinatesOutOfBoundsException extends RuntimeException {
+    
+}

src/was/GameBoard.java
@@ -147,7 +147,7 @@ public class GameBoard {
         }
         log(player + " " + player.getLocation() + " " + m);
 
-        if (player != board.get(getIndex(player.getLocation()))) {
+        if (player != board.get(getIndexUnchecked(player.getLocation()))) {
             System.err.println("player " + player + "location mismatch. player thinks its at " + player.getLocation() + " while board has something else there.. ");
             System.err.println("The board has, at " + player.getLocation() + ":" + board.get(getIndex(player.getLocation())));
         }
@@ -158,7 +158,7 @@ public class GameBoard {
 
         int x = loc.x;
         int y = loc.y;
-        int i = getIndex(x, y);
+        int i = getIndexUnchecked(x, y);
 
         x += (int) m.delta_x;
         y += (int) m.delta_y;
@@ -167,7 +167,7 @@ public class GameBoard {
         x = Math.min(cols - 1, x);
         y = Math.min(rows - 1, y);
 
-        int idx = getIndex(x, y);
+        int idx = getIndexUnchecked(x, y);
 
         if (idx == i) {
             return true; // empty move
@@ -177,7 +177,7 @@ public class GameBoard {
         x = nloc.x;
         y = nloc.y;
 
-        idx = getIndex(x, y);
+        idx = getIndexUnchecked(x, y);
         if (idx == i) {
             return true; // empty move
         }
@@ -306,10 +306,20 @@ public class GameBoard {
     }
 
     int getIndex(GameLocation l) {
-        return l.y * cols + l.x;
+        return getIndex(l.x, l.y);
     }
 
     int getIndex(int x, int y) {
+        if (x < 0 || x >= cols || y < 0 || y >= rows) {
+            throw new CoordinatesOutOfBoundsException();
+        }
+        return getIndexUnchecked(x,y);
+    }
+    
+    int getIndexUnchecked(GameLocation l) {
+        return getIndexUnchecked(l.x, l.y);
+    }
+    int getIndexUnchecked(int x, int y) {
         return y * cols + x;
     }
 
@@ -404,7 +414,7 @@ public class GameBoard {
         if (loc.x < 0 || loc.x >= cols || loc.y < 0 || loc.y >= rows) {
             return null;
         }
-        int idx = getIndex(loc.x, loc.y);
+        int idx = getIndexUnchecked(loc.x, loc.y);
         Player p = getPlayer(idx);
         if (p != null) {
             return p.getID();
@@ -519,11 +529,11 @@ public class GameBoard {
             return GamePiece.OBSTACLE;  // out of bounds - instead of throwing an Exception
         }
 
-        return getPiece(getIndex(x, y));
+        return getPiece(getIndexUnchecked(x, y));
     }
 
     GamePiece getPiece(GameLocation i) {
-        return getPiece(getIndex(i.x, i.y));
+        return getPiece(i.x, i.y);
     }
 
     GamePiece getPiece(int i) {
@@ -803,7 +813,7 @@ public class GameBoard {
         // let's make sure there's no cell left
 
         log("removing player.");
-        int i = getIndex(p.getLocation());
+        int i = getIndexUnchecked(p.getLocation());
         if (board.get(i) == p) {
             board.set(i, null);
         }

commit 4273193a9bcc992dbf1d153039cb822bb6232012

Fix scenarios constructed with negative ordinates Fix bug introduced in 0f48de0

src/was/Scenario.java
@@ -311,13 +311,11 @@ public class Scenario {
         try {
             GameLocation l = loc(x, y);
             if (l != null) {
-                if (x > 0 && x < tmpGb.getCols() && y > 0 && y < tmpGb.getRows()) {
 
-                    if (tmpGb.isEmptyCell(l.x, l.y)) {
-                        return tmpGb.addPlayer((Player) p.newInstance(), l);
-                    }
-
+                if (tmpGb.isEmptyCell(l.x, l.y)) {
+                    return tmpGb.addPlayer((Player) p.newInstance(), l);
                 }
+
             }
         } catch (CoordinatesOutOfBoundsException ex) {
         } catch (IndexOutOfBoundsException ex) {